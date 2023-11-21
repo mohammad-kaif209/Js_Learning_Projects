e12c3bc4f536e342adb51f8f55d37a6e25db2bb4
mohammad-kaif209 <99971624+mohammad-kaif209@users.noreply.github.com>
2023-11-21 15:04:22 +0000

    Strings and Methods

---
+++ 01_Basics/String.js
@@ -0,0 +1,16 @@
+/*
+string can be defined in between ' ' && " " in javascript.
+*/
+
+const name = "kaif";
+const age = 20;
+console.log(name+age);
+
+console.log(`hello my name is ${name} and my age is ${age}`); //modern way of printing
+
+//another of declaring string
+let email = new String("mohammedkaif209@gmail.com");
+console.log(email);
+
+console.log(typeof name);
+console.log(typeof email);
