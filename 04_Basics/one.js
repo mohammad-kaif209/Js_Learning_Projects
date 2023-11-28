4ccefc6d75618d470744e3a448b4f9ae724f92a7
mohammad-kaif209 <99971624+mohammad-kaif209@users.noreply.github.com>
2023-11-28 15:05:23 +0000

    new

--- 04_Basics/one.js
+++ 04_Basics/one.js
@@ -4,3 +4,13 @@
 //falsy values
 
 //false,BIgInt 0n , 0 , "" , NaN, -0 , null,undefined.
+
+//truthy values
+
+// "0" , 'false' , " " , {},[],function(){}
+
+const myObj = {};
+//to get object length we use
+if((Object.keys(myObj).length) == 0 ){
+    console.log("is empty");
+}
