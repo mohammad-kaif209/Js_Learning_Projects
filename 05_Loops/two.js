906d34f72081d11fe34f64938f7a71187423c7c6
mohammad-kaif209 <99971624+mohammad-kaif209@users.noreply.github.com>
2023-11-30 15:44:57 +0000

    about while

---
+++ 05_Loops/two.js
@@ -0,0 +1,14 @@
+//about while and do while loops
+
+
+let val = 20;
+while(val>=0){
+    console.log(`value is ${val}`);
+    val--;
+}
+console.log("helloooo");
+let val1=9;
+do {
+    console.log(`value is ${val1}`);
+    val1--;
+} while (val1>=10);
