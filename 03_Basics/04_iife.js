f842b96954b3ffc28dd0aa2fc9371467669e3af9
Shaik Mohammad Kaif <99971624+mohammad-kaif209@users.noreply.github.com>
2024-08-30 06:26:57 +0000

    Only_Projects

--- 03_Basics/04_iife.js
+++
@@ -1,9 +0,0 @@
-//immediately invoked function expression (IIFE)
-(function myFunc(){
-    console.log(`db connected...`);  //named iife
-})();  //";" is to terminate function calling
-
-((name)=>{
-    console.log(`db is not connected Mr.${name}`);
-    //unnamed iife
-})("kaif");
--- 08_events/script.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world!!");
-
-setTimeout(()=>{
-    console.log("0");
-},0);
-
-console.log("1");
